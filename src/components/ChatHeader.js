3ea34514b07ae51781f229695d4320ac210ae109
Huinko <egor.markin.2019@mail.ru>
2023-03-13 02:56:49 +0500

    final front

---
+++ src/components/ChatHeader.js
@@ -0,0 +1,15 @@
+const ChatHeader = () => {
+    return (
+        <div className="chat-container-header">
+            <duv className="profile">
+                <div className="img-container">
+                    <img src="" alt="" />
+                </div>
+                <h3>UserName</h3>
+            </duv>
+            <i className="log-out-icon">←</i>
+        </div>
+    )
+}
+
+export default ChatHeader;
